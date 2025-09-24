6b92ab7c418aa8aeed872e56b4731d6065d5c3ab
MahendraprasadR <iammahendraprasadr@gmail.com>
2025-09-24 11:53:43 +0530

    Add SelectionSort implementation

---
+++ SelectionSort.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 10, 5, 25, 1, 8, 79 };
+        selectionSort(arr);
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
