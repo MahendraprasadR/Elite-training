eeabf47a14f5dfb5081fb7f74bba5352d3e73f0b
MahendraprasadR <iammahendraprasadr@gmail.com>
2025-09-26 10:10:25 +0530

    Refactor LinkedListUsingBuiltin: replace push/pop with add/remove methods for LinkedList operations

--- LinkedListUsingBuiltin.java
+++ LinkedListUsingBuiltin.java
@@ -1,19 +1,25 @@
 import java.util.LinkedList;
+
 public class LinkedListUsingBuiltin {
-public static void main(String[] args) {
-    LinkedList<Integer> list = new LinkedList();
-    list.push(10);
-     list.push(20);
-     list.push(30);
-     list.push(40);
-     list.push(50);
-     System.out.println(list);
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
 
-     list.pop();
-     System.out.println(list);
+        list.add(10);
+        list.add(100);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        System.out.println(list);
 
-     System.out.println(list.peek());
-     System.out.println(list.size());
-     System.out.println(list.isEmpty());
-}
+        list.removeFirst();
+        System.out.println(list);
+
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+
+        System.out.println(list.size());
+
+        System.out.println(list.isEmpty());
+    }
 }
